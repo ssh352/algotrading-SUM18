5be1d3863ae9c3968594203e559f0eabaf196306
pejato <petjactol@gmail.com>
2018-07-11 13:28:14 -0400

    Did an auto format with Pycharm which adjusted some spacing stuff

---
+++ CBP_adaptions.py
@@ -0,0 +1,150 @@
+
+from websocket import *
+import time
+import threading
+import six
+
+
+COINBASE_PRO_MAX_QUERY_FREQ = 4  # Highest frequency we're allowed to query at before potential IP ban
+
+
+class HeartbeatSilentException(Exception):
+    """
+    Raised when the websocket has not received the heartbeat message for an extended time
+    """
+    pass
+
+
+# Simple modification of the default run_forever() method, which transfers timeout handling from ping-pong to
+# heartbeat messages (coinbase does not seem to support ping-pong)
+class CoinbaseProAdaptedWS(WebSocketApp):
+    def __init__(self, heartbeat_timeout=10, *args, **kwargs):
+        super(CoinbaseProAdaptedWS, self).__init__(*args, **kwargs)
+        self.last_heartbeat = time.time()
+        self.heartbeat_timeout = heartbeat_timeout
+
+    def run_forever(self, sockopt=None, sslopt=None,
+                    ping_interval=0, ping_timeout=None,
+                    http_proxy_host=None, http_proxy_port=None,
+                    http_no_proxy=None, http_proxy_auth=None,
+                    skip_utf8_validation=False,
+                    host=None, origin=None, dispatcher=None):
+        """
+        run event loop for WebSocket framework.
+        This loop is infinite loop and is alive during websocket is available.
+        sockopt: values for socket.setsockopt.
+            sockopt must be tuple
+            and each element is argument of sock.setsockopt.
+        sslopt: ssl socket optional dict.
+        ping_interval: automatically send "ping" command
+            every specified period(second)
+            if set to 0, not send automatically.
+        ping_timeout: timeout(second) if the pong message is not received.
+        http_proxy_host: http proxy host name.
+        http_proxy_port: http proxy port. If not set, set to 80.
+        http_no_proxy: host names, which doesn't use proxy.
+        skip_utf8_validation: skip utf8 validation.
+        host: update host header.
+        origin: update origin header.
+        """
+
+        if not ping_timeout or ping_timeout <= 0:
+            ping_timeout = None
+        if ping_timeout and ping_interval and ping_interval <= ping_timeout:
+            raise WebSocketException("Ensure ping_interval > ping_timeout")
+        if sockopt is None:
+            sockopt = []
+        if sslopt is None:
+            sslopt = {}
+        if self.sock:
+            raise WebSocketException("socket is already opened")
+        thread = None
+        close_frame = None
+        self.keep_running = True
+        self.last_ping_tm = 0
+        self.last_pong_tm = 0
+
+        def teardown():
+            if thread and thread.isAlive():
+                event.set()
+                thread.join()
+            self.keep_running = False
+            self.sock.close()
+            close_args = self._get_close_args(
+                close_frame.data if close_frame else None)
+            self._callback(self.on_close, *close_args)
+            self.sock = None
+
+        try:
+            self.sock = WebSocket(
+                self.get_mask_key, sockopt=sockopt, sslopt=sslopt,
+                fire_cont_frame=self.on_cont_message and True or False,
+                skip_utf8_validation=skip_utf8_validation)
+            self.sock.settimeout(getdefaulttimeout())
+            self.sock.connect(
+                self.url, header=self.header, cookie=self.cookie,
+                http_proxy_host=http_proxy_host,
+                http_proxy_port=http_proxy_port, http_no_proxy=http_no_proxy,
+                http_proxy_auth=http_proxy_auth, subprotocols=self.subprotocols,
+                host=host, origin=origin)
+            if not dispatcher:
+                dispatcher = self.create_dispatcher(ping_timeout)
+
+            self._callback(self.on_open)
+
+            if ping_interval:
+                event = threading.Event()
+                thread = threading.Thread(
+                    target=self._send_ping, args=(ping_interval, event))
+                thread.setDaemon(True)
+                thread.start()
+
+            def read():
+                if not self.keep_running:
+                    return teardown()
+
+                op_code, frame = self.sock.recv_data_frame(True)
+                if op_code == ABNF.OPCODE_CLOSE:
+                    close_frame = frame
+                    return teardown()
+                elif op_code == ABNF.OPCODE_PING:
+                    self._callback(self.on_ping, frame.data)
+                elif op_code == ABNF.OPCODE_PONG:
+                    self.last_pong_tm = time.time()
+                    self._callback(self.on_pong, frame.data)
+                elif op_code == ABNF.OPCODE_CONT and self.on_cont_message:
+                    self._callback(self.on_data, frame.data,
+                                   frame.opcode, frame.fin)
+                    self._callback(self.on_cont_message,
+                                   frame.data, frame.fin)
+                else:
+                    data = frame.data
+                    if six.PY3 and op_code == ABNF.OPCODE_TEXT:
+                        data = data.decode("utf-8")
+                    self._callback(self.on_data, data, frame.opcode, True)
+                    self._callback(self.on_message, data)
+
+                return True
+
+            def check():
+                if ping_timeout and self.last_ping_tm \
+                        and time.time() - self.last_ping_tm > ping_timeout \
+                        and self.last_ping_tm - self.last_pong_tm > ping_timeout:
+                    raise WebSocketTimeoutException("ping/pong timed out")
+                dur = time.time() - self.last_heartbeat
+                if dur > self.heartbeat_timeout:
+                    raise HeartbeatSilentException("No heartbeat for {} seconds".format(dur))
+                return True
+
+            dispatcher.read(self.sock.sock, read, check)
+
+        # I've shuffled some of the exception handling to better suit our needs
+        except (KeyboardInterrupt, SystemExit) as e:
+            self._callback(self.on_error, e)
+            if isinstance(e, SystemExit):
+                # propagate SystemExit further
+                raise
+            teardown()
+        except Exception as e:
+            self._callback(self.on_error, e)
+            teardown()
--- ws.py
+++ ws.py
@@ -3,51 +3,58 @@
 # websocket dependency VVV
 # https://pypi.org/project/websocket-client/
 
-from websocket import WebSocketApp
+import time
+import logging
+import os
+
+from CBP_adaptions import CoinbaseProAdaptedWS, COINBASE_PRO_MAX_QUERY_FREQ
 from json import dumps, loads
 from pprint import pprint
 
 
-def on_open(ws):
+def on_open(ws: CoinbaseProAdaptedWS):
     params = {
         "type": "subscribe",
         "product_ids": [
-            "BTC-USD",
-            "ETH-USD"
+            "BTC-USD"
         ],
         "channels": [
             "level2",
             "heartbeat",
-            {
-                "name": "ticker",
-                "product_ids": [
-                    "BTC-USD",
-                    "ETH-USD"
-                ]
-            }
         ]
     }
     ws.send(dumps(params))
 
-    print("######### Coinbase connection opened #########")
+    logging.info("Coinbase connection opened")
 
 
-def on_message(ws, message):
-    pprint(loads(message))
+def on_message(ws: CoinbaseProAdaptedWS, message: str):
+    j = loads(message)
+    if j["type"] == "heartbeat":
+        ws.last_heartbeat = time.time()
+    pprint(j)
 
 
-def on_error(ws, error):
-    print(error)
+def on_error(ws: CoinbaseProAdaptedWS, error):
+    logging.warning(error)
+    time.sleep(COINBASE_PRO_MAX_QUERY_FREQ)
+    logging.warning("Attempting reconnect")
 
 
-def on_close(ws):
-    print("######### Coinbase connection closed #########")
+def on_close(ws: CoinbaseProAdaptedWS):
+    logging.info("Coinbase connection closed")
 
 
 if __name__ == "__main__":
-    ws = WebSocketApp("wss://ws-feed.pro.coinbase.com",
-                      on_open=on_open,
-                      on_message=on_message,
-                      on_error=on_error,
-                      on_close=on_close)
-    ws.run_forever()
+    logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"),
+                        format="%(asctime)s;%(levelname)s;%(message)s")
+    ws = CoinbaseProAdaptedWS(url="wss://ws-feed.pro.coinbase.com",
+                              on_open=on_open,
+                              on_message=on_message,
+                              on_error=on_error,
+                              on_close=on_close)
+    try:
+        while True:
+            ws.run_forever()
+    except (KeyboardInterrupt, SystemExit) as e:
+        logging.info("{} Interrupt processed, exiting...".format(type(e).__name__))
